cbdc046ee6ac9342b343fe3b44c0b29879908bd0
Ludek Benedik <ludek.benedik@gmail.com>
2025-06-23 10:15:34 +0200

    EasyAdminExtension extends Symfony\Component\DependencyInjection\Extension\Extension instead of Symfony\Component\HttpKernel\DependencyInjection\Extension

--- src/DependencyInjection/EasyAdminExtension.php
+++ src/DependencyInjection/EasyAdminExtension.php
@@ -4,8 +4,8 @@ namespace EasyCorp\Bundle\EasyAdminBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Resolves all the backend configuration values and most of the entities
